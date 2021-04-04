441142f2b7eacc347e1e459db637a5294dc08049
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-04-05 00:29:14 +0300

    fetch request + firebase

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,14 +1,11 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import { ADD_TODO, CLEAR_ERROR, FETCH_TODOS, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO } from '../types'
 
 const handlers = {
-  [ADD_TODO]: (state, { title }) => ({
+  [ADD_TODO]: (state, { title, id}) => ({
     ...state,
     todos: [
       ...state.todos,
-      {
-        id: Date.now().toString(),
-        title
-      }
+      { id, title }
     ]
   }),
   [REMOVE_TODO]: (state, { id }) => ({
@@ -24,6 +21,12 @@ const handlers = {
       return todo
     })
   }),
+  [SHOW_LOADER]: (state) => ({ ...state, loading: true }),
+  [HIDE_LOADER]: (state) => ({ ...state, loading: false }),
+  [CLEAR_ERROR]: (state) => ({ ...state, error: null }),
+  [SHOW_ERROR]: (state, {error}) => ({...state, error: error}),
+  [FETCH_TODOS]: (state, {todos}) => ({...state, todos}),
+
   DEFAULT: state => state
 }
 
